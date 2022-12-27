b691949be77df2517c500c0f8ee3a6e06a059807
Alex Proskuriakov <alexpro1972@yandex.ru>
2022-12-27 16:11:14 +0300

    Added type hints

--- pep_parse/constants.py
+++ pep_parse/constants.py
@@ -1,8 +1,18 @@
 from pathlib import Path
-from typing import Final
 
+
+BASE_DIR = Path(__file__).parent.parent / 'results'
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+DIALECT = 'unix'
+ENCODING = 'utf-8'
+EXT = 'csv'
+
+
+'''
+На ЯП не проходит тесты так как там Python 3.7
 BASE_DIR: Final = Path(__file__).parent.parent / 'results'
 DATETIME_FORMAT: Final = '%Y-%m-%d_%H-%M-%S'
 DIALECT: Final = 'unix'
 ENCODING: Final = 'utf-8'
 EXT: Final = 'csv'
+'''
